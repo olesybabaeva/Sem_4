7252f9d9d5813b3ea6f1e4c277adf63178a31537
Lesya <olesybabaeva@gmail.com>
2024-12-11 14:57:27 +0300

    задания
    добавлены

---
+++ Zad_1.py
@@ -0,0 +1,7 @@
+﻿#Напишите функцию для транспонирования матрицы
+
+matrix = [[1, 2, 3], [5, 6, 7]]
+print(matrix)
+
+t_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+print(t_matrix)
---
+++ Zad_2.py
@@ -0,0 +1,14 @@
+﻿#Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, 
+# где ключ — значение переданного аргумента, а значение — имя аргумента. 
+# Если ключ не хешируем, используйте его строковое представление.
+
+def kwargs_to_dict(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        try:
+            result[value] = key
+        except:
+            result[str(value)] = key
+    return result
+
+print(kwargs_to_dict(cat='kisa', turtle=['jack', 'misha', 'bobik'], dog={'bob': 2, 'bib': 3}))
